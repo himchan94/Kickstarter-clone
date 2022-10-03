304b0bf578201b13214d8d3033433ef96d3a43e1
himchan94 <himchan9494@gmail.com>
2022-10-04 02:53:30 +0900

    feat: Change folder structure

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,7 @@
 import express = require("express");
-import { connectDB } from "../database/db";
-import { errorHandler } from "../middleware/errorMiddleware";
-import { PORT } from "../utils/config";
+import { connectDB } from "./database/db";
+import { errorHandler } from "./middleware/errorMiddleware";
+import { PORT } from "./utils/config";
 import * as Colors from "colors.ts";
 Colors.colors("", "");
 
@@ -10,7 +10,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.use("/api/projects", require("../routes/projectRoutes"));
+app.use("/api/projects", require("./routes/projectRoutes"));
 
 app.use(errorHandler);
 
